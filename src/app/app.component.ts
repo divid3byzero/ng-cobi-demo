f591b508964ab12b7516ad7a7e80fcb0da8cf8dc
Benedikt Buchner <benedikt.buchner@solvimo.de>
2020-04-21 22:03:46 +0200

    - Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-cobi-demo';
+export class AppComponent implements OnInit {
+
+  public currentSpeed: number;
+
+  ngOnInit(): void {
+    COBI.init('token');
+    setTimeout(() => {
+      COBI.rideService.speed.subscribe(speed => this.currentSpeed = speed);
+    }, 2000)
+
+  }
 }
